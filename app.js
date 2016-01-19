767ac69450513f163a1c7b3e5eef49dc9f171a28
Phan Quoc Bao <o0o.baoquocphan.o0o@gmail.com>
2016-01-19 19:12:18 +0700

    Add point text, refactor code

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 rand = Math.random;
 
+var type_color = { p1 : '#FAE639', p2 : 'white' };
+var type_str = { p1 : 'Yellow', p2 : 'White' };
+
 var ww = 840, wh = 480;
-var stage = null;
+var stage, cur_turn;
 
-var cur_turn;
-
-var player1_dots = [], player2_dots = [];
-
-var type_color = {
-  player1 : '#FAE639',
-  player2 : 'white'
-};
-
-var type_str = {
-  player1 : 'Yellow',
-  player2 : 'White'
-};
+var p1_dots = [], p2_dots = [];
+var p1_point, p2_point;
+var p1_point_text, p2_point_text;
 
 function Dot(type, x, y) {
   this.type = type;
@@ -112,26 +105,27 @@ Dot.prototype.hide_ring = function(event) {
   if (!(new_x < 0 || new_x > ww || new_y < 0 || new_y > wh)) {
     var new_dot = new Dot(this.type, this.circle.x, this.circle.y);
     new_dot.begin_draw();
-    if (this.type == 'player1')
-      player1_dots.push(new_dot);
+    if (this.type == 'p1')
+      p1_dots.push(new_dot);
     else
-      player2_dots.push(new_dot);
+      p2_dots.push(new_dot);
 
     createjs.Tween.get(new_dot.circle, { loop : false })
       .to({ x : new_x, y : new_y }, 200, createjs.Ease.sineOut());
   }
 
   var ene;
-  if (this.type == 'player1')
-    ene = player2_dots;
+  if (this.type == 'p1')
+    ene = p2_dots;
   else
-    ene = player1_dots;
+    ene = p1_dots;
 
   var seg = new Segment(
     new Vector2(this.circle.x, this.circle.y),
     new Vector2(new_x, new_y)
   );
 
+  var dpoint = 0;
   for (i = 0; i < ene.length; ++i)
     if (
       !ene[i].died &&
@@ -142,19 +136,96 @@ Dot.prototype.hide_ring = function(event) {
         .beginFill('gray')
         .drawCircle(0, 0, 6)
         .endFill();
-      ene.splice(i, 1); i--;
+      ene.splice(i, 1); i--; dpoint++;
     }
 
-  if (player1_dots.length == 0)
-    show_notification(type_str['player2'] + ' win!');
-  else if (player2_dots.length == 0)
-    show_notification(type_str['player1'] + ' win!');
+  if (p1_dots.length == 0)
+    game_over('p2');
+  else if (p2_dots.length == 0)
+    game_over('p1');
 
-  if (cur_turn == 'player1')
-    cur_turn = 'player2';
-  else
-    cur_turn = 'player1';
+  if (cur_turn == 'p1') {
+    p1_point += dpoint;
+    cur_turn = 'p2';
+    p1_point_text.shadow.blur = 0;
+    p2_point_text.shadow.blur = 8;
+  } else {
+    p2_point += dpoint;
+    cur_turn = 'p1';
+    p1_point_text.shadow.blur = 8;
+    p2_point_text.shadow.blur = 0;
+  }
+
+  p1_point_text.text = p1_point.toString();
+  p2_point_text.text = p2_point.toString();
 };
+
+function start_game() {
+  stage.removeAllChildren();
+
+  p1_point = p2_point = 0;
+  p1_point_text =
+    new createjs.Text('0', '24px Playfair Display', type_color['p1']);
+  p2_point_text =
+    new createjs.Text('0', '24px Playfair Display', type_color['p2']);
+  p1_point_text.y = p2_point_text.y = 10;
+  p1_point_text.x = 10; p2_point_text.x = 64;
+  p1_point_text.shadow =
+    p2_point_text.shadow || new createjs.Shadow(type_color['p1'], 0, 0, 8);
+  p2_point_text.shadow =
+    p2_point_text.shadow || new createjs.Shadow(type_color['p2'], 0, 0, 0);
+  stage.addChild(p1_point_text);
+  stage.addChild(p2_point_text);
+
+  cur_turn = 'p1';
+
+  p1_dots[0] = new Dot('p1', rand()*ww, rand()*wh);
+  p1_dots[0].begin_draw();
+  p2_dots[0] = new Dot('p2', rand()*ww, rand()*wh);
+  p2_dots[0].begin_draw();
+
+  for (i = 0; i < 100; ++i) {
+    var star = new createjs.Shape();
+    star.graphics
+      .beginFill('white')
+      .drawCircle(0, 0, rand()*2)
+      .endFill();
+    star.x = rand()*ww;
+    star.y = rand()*wh;
+    star.alpha = 0;
+    createjs.Tween.get(star, { loop : true })
+      .to({ alpha : rand() }, 10000, createjs.Ease.linear)
+      .to({ alpha : 0 }, 10000, createjs.Ease.linear)
+      .to({ x : rand()*ww, y : rand()*wh }, 100, createjs.Ease.none);
+    stage.addChild(star);
+  }
+}
+
+function game_over(type) {
+  var text = new createjs.Text(
+    type_str[type] + ' win!', '48px Playfair Display', type_color[type]);
+  text.x = ww/2; text.y = wh/2 - 24; text.textAlign = 'center';
+  text.shadow = new createjs.Shadow(type_color[type], 0, 0, 8);
+  text.addEventListener('click', start_game);
+  stage.addChild(text);
+}
+
+function take_a_screenshot() {
+  var canvas = document.getElementById('app');
+  var img = canvas.toDataURL("image/png", 1.0);
+  window.open(img, '');
+}
+
+// Src: http://stackoverflow.com/questions/16124569/how-can-i-make-the-html5-canvas-go-fullscreen
+function goFullScreen(){
+  var canvas = document.getElementById('app');
+  if(canvas.requestFullScreen)
+    canvas.requestFullScreen();
+  else if(canvas.webkitRequestFullScreen)
+    canvas.webkitRequestFullScreen();
+  else if(canvas.mozRequestFullScreen)
+    canvas.mozRequestFullScreen();
+}
 
 function app() {
   stage = new createjs.Stage('app');
