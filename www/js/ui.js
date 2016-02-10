d9dd24b376a148bff5ce51e3b650439a71763304
Phan Quoc Bao <o0o.baoquocphan.o0o@gmail.com>
2016-02-10 15:02:42 +0700

    Fix js

--- www/js/ui.js
+++ www/js/ui.js
@@ -34,7 +34,7 @@ function take_a_screenshot() {
 function show_game() {
   document.getElementById('welcome').style.display = 'none';
   window.scrollY = 0;
-  document.onclick = show_menu;
+  window.onclick = show_menu;
 }
 
 setInterval(function() {
